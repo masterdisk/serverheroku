ca164219d619197eb2dc8e88db25933726f25a3e
masterdisk <costeaeduardnic@gmail.com>
2021-08-24 14:32:54 +0200

    -added Tokens for Visma -added GET:customers -added GET:orders -added GET:invoices

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // server/server/js
 const express = require("express");
+const axios = require("axios");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8000;
+const CircularJSON = require('circular-json');
 
 app.use(bodyParser.json());
 app.use(cors({
@@ -50,7 +52,94 @@ app.get("/events", (req, res) => {
     res.send(events);
 });
 
-app.get("/", (req, res) => {
+app.get("/visma/demo", async (req, res, next) => {
+
+    try {
+        const apicall = await axios.get(("https://restapi.e-conomic.com/customers"), {
+            headers: {
+                'X-AppSecretToken': "demo",
+                'X-AgreementGrantToken': "demo",
+                'Content-Type': "application/json"
+            }
+        });
+
+        console.log(apicall.data);
+        res.send(apicall.data);
+    } catch (err) {
+        console.log(err);
+    }
+}) //template visma GET - HTTPS Request
+
+app.get("/visma/customers", async (req, res, next) => {
+
+    try {
+        const apicall = await axios.get(("https://restapi.e-conomic.com/customers"), {
+            headers: {
+                'X-AppSecretToken': "3BHJkhRDuI1VRQr03bJm6pGPukQ8EhWjgGfMdFfEef41",
+                'X-AgreementGrantToken': "oi1YjRUh16ZGAuNSAwRlmnHvEtyPedUBN02xl3B4Yuo1",
+                'Content-Type': "application/json"
+            }
+        });
+        res.send(apicall.data);
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
+app.get("/visma/orders", async (req, res, next) => {
+
+    try {
+        const apicall = await axios.get(("https://restapi.e-conomic.com/orders"), {
+            headers: {
+                'X-AppSecretToken': "3BHJkhRDuI1VRQr03bJm6pGPukQ8EhWjgGfMdFfEef41",
+                'X-AgreementGrantToken': "oi1YjRUh16ZGAuNSAwRlmnHvEtyPedUBN02xl3B4Yuo1",
+                'Content-Type': "application/json"
+            }
+        });
+        res.send(apicall.data);
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
+app.get("/visma/invoices", async (req, res, next) => {
+
+    try {
+        const apicall = await axios.get(("https://restapi.e-conomic.com/invoices/booked"), {
+            headers: {
+                'X-AppSecretToken': "3BHJkhRDuI1VRQr03bJm6pGPukQ8EhWjgGfMdFfEef41",
+                'X-AgreementGrantToken': "oi1YjRUh16ZGAuNSAwRlmnHvEtyPedUBN02xl3B4Yuo1",
+                'Content-Type': "application/json"
+            }
+        });
+        res.send(apicall.data);
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
+app.get("/visma/", async (req, res, next) => {
+
+    try {
+        const apicall = await axios.get(("https://restapi.e-conomic.com/customers"), {
+            headers: {
+                'X-AppSecretToken': "demo",
+                'X-AgreementGrantToken': "demo",
+                'Content-Type': "application/json"
+            }
+        });
+
+        console.log(apicall.data);
+        res.send(apicall.data);
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+app.get("/echo", (req, res) => {
     res.send(`Hi! Server is listening on port ${port}`);
 });
 
