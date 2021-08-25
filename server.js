263a9c3f4486b5c0934f5d51535c1bdfa0ee88ce
masterdisk <costeaeduardnic@gmail.com>
2021-08-25 09:43:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 
 
